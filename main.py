fc832895dcca2d6ca435a57d71f6b05df06154e1
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-12-06 00:56:50 +0900

    Данные берутся и отправляются в телегу. Скрэйпер не хочет работать отдельным файлом

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import datetime
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -11,10 +15,61 @@ bot = Bot(token=bot_token.read())
 # Диспетчер
 dp = Dispatcher()
 
+urls = []
+titles = []
+
+website = 'https://www.s-vfu.ru/universitet/rukovodstvo-i-struktura/instituty/imi/news/?PAGEN_1=1'
+cService = webdriver.ChromeService(executable_path='./chromedriver.exe')
+driver = webdriver.Chrome(service=cService)
+
+def scrape_today():
+    #time_now = datetime.datetime.now()
+    time_now = datetime.datetime.strptime('27.11.2023 16:00:00', '%d.%m.%Y %H:%M:%S')
+    current_page = website
+    for i in range(1, 28):
+        current_page = current_page.replace('?PAGEN_1=' + current_page[-1], '?PAGEN_1=' + str(i))
+        driver.get(current_page)
+        articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
+        urls = []
+        titles = []
+        for j in range(2, len(articles)):
+            article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div[1]/a[1]").get_attribute('href')
+            driver.get(article)
+        
+            date = driver.find_element(By.XPATH, "//*[@id='content']/article/ul/li[2]/kbd").get_attribute('innerText')
+            date_datetime = datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+
+            if (time_now >= date_datetime and time_now.day != date_datetime.day):
+                print('urls',urls)
+                print('titles',titles)
+                return urls, titles
+            
+            urls.append(article)
+            
+            if driver.find_elements(By.XPATH, "//*[@id='content']/section"):
+                title_image = driver.find_element(By.XPATH, "//*[@id='content']/section").get_attribute("style").replace('background-image: url("', '').replace('");', '')
+                title_image = "https://www.s-vfu.ru" + title_image
+                #print(title_image)
+
+            title = driver.find_element(By.XPATH, "/html/head/title").get_attribute('innerText')
+            titles.append(title)
+            #print(title)
+            driver.get(current_page)
+
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+
+@dp.message(Command("startscrape"))
+async def cmd_start(message: types.Message):
+    global urls, titles
+    urls, titles = scrape_today()
+
+@dp.message(Command("scraperesult"))
+async def cmd_start(message: types.Message):
+    await message.answer(str(titles[0]) + '\n' + str(urls[0]))
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
@@ -22,3 +77,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+driver.quit()
--- scraper.py
+++ scraper.py
@@ -1,33 +1,120 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import datetime
 
 # Стартовая страница
 website = 'https://www.s-vfu.ru/universitet/rukovodstvo-i-struktura/instituty/imi/news/?PAGEN_1=1'
-driver = webdriver.Chrome()
-driver.get(website)
+cService = webdriver.ChromeService(executable_path='./chromedriver.exe')
+driver = webdriver.Chrome(service=cService)
+#driver.get(website)
 
 # Кол-во страниц
-pages = int(driver.find_element(By.XPATH, "//*[@id='content']/div[2]/div/ul/li[6]/a/span").get_attribute('innerText'))
-print(str(pages))
+#pages = int(driver.find_element(By.XPATH, "//*[@id='content']/div[2]/div/ul/li[6]/a/span").get_attribute('innerText'))
+pages = 28
+#print('pages: ' + str(pages))
 
 # Получаем url страниц самих новостей
-article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div[2]/article/a[1]").get_attribute('href')
-driver.get(article)
+#articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
+#article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div[2]/article/a[1]").get_attribute('href')
 
-# Получаем нужные данные (титульник, текст, изображения, если они присутствуют)
-title = driver.find_element(By.XPATH, "/html/head/title").get_attribute('innerText')
-print(title)
+def scrape_all():
+    current_page = website
+    for i in range(1, pages):
+        current_page = current_page.replace('?PAGEN_1=' + current_page[-1], '?PAGEN_1=' + str(i))
+        driver.get(current_page)
+        articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
+        for j in range(2, len(articles)):
+            #scrape_article(articles[i].find_element(By.XPATH, '//article/a[1]').get_attribute('href'))
+            scrape_article(article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div[1]/a[1]").get_attribute('href'))
+            driver.get(current_page)
 
-title_image = driver.find_element(By.XPATH, "//*[@id='content']/section").get_attribute("style").replace('background-image: url(', '').replace(');', '')
-print(title_image)
+def scrape_article(article):
+    driver.get(article)
 
-text = driver.find_elements(By.XPATH, "//*[@id='content']/article/div/p")
-for i in range(len(text)):
-    print(text[i].get_attribute('innerText'))
+    # Получаем нужные данные (титульник, текст, изображения, если они присутствуют)
+    title = driver.find_element(By.XPATH, "/html/head/title").get_attribute('innerText')
+    print(title)
 
-images = driver.find_elements(By.XPATH, "//*[@id='content']/div/div/div")
-if images:
-    for i in range(len(images)):
-        print(images[i].find_element(By.TAG_NAME, 'a').get_attribute('href'))
+    if driver.find_elements(By.XPATH, "//*[@id='content']/section"):
+        title_image = driver.find_element(By.XPATH, "//*[@id='content']/section").get_attribute("style").replace('background-image: url("', '').replace('");', '')
+        title_image = "https://www.s-vfu.ru" + title_image
+        print(title_image)
+
+    text = driver.find_elements(By.XPATH, "//*[@id='content']/article/div/p")
+    for i in range(len(text)):
+        print(text[i].get_attribute('innerText'))
+
+    images = driver.find_elements(By.XPATH, "//*[@id='content']/div/div/div")
+    if images:
+        for i in range(len(images)):
+            print(images[i].find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+def scrape_all_min():
+    current_page = website
+    for i in range(1, pages):
+        current_page = current_page.replace('?PAGEN_1=' + current_page[-1], '?PAGEN_1=' + str(i))
+        driver.get(current_page)
+        articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
+        for j in range(2, len(articles)):
+            image = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/a/img")
+            if image:
+                print(image[0].get_attribute('src'))
+
+            title = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div/h3/a")
+            if title:
+                print(title[0].get_attribute('innerText'))
+
+            else:
+                title = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/h3/a")
+                if title:
+                    print(title[0].get_attribute('innerText'))
+
+                else:
+                    title = "No title"
+                    print(title)
+
+            date = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/figcaption/a").get_attribute('innerText')
+            print(date)
+
+            url = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div/a").get_attribute('href')
+            print(url)
+
+def scrape_today():
+    #time_now = datetime.datetime.now()
+    time_now = datetime.datetime.strptime('27.11.2023 16:00:00', '%d.%m.%Y %H:%M:%S')
+    current_page = website
+    for i in range(1, pages):
+        current_page = current_page.replace('?PAGEN_1=' + current_page[-1], '?PAGEN_1=' + str(i))
+        driver.get(current_page)
+        articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
+        urls = []
+        titles = []
+        for j in range(2, len(articles)):
+            article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div[1]/a[1]").get_attribute('href')
+            driver.get(article)
+        
+            date = driver.find_element(By.XPATH, "//*[@id='content']/article/ul/li[2]/kbd").get_attribute('innerText')
+            date_datetime = datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+
+            if (time_now >= date_datetime and time_now.day != date_datetime.day):
+                print('urls',urls)
+                print('titles',titles)
+                return urls, titles
+            
+            urls.append(article)
+            
+            if driver.find_elements(By.XPATH, "//*[@id='content']/section"):
+                title_image = driver.find_element(By.XPATH, "//*[@id='content']/section").get_attribute("style").replace('background-image: url("', '').replace('");', '')
+                title_image = "https://www.s-vfu.ru" + title_image
+                #print(title_image)
+
+            title = driver.find_element(By.XPATH, "/html/head/title").get_attribute('innerText')
+            titles.append(title)
+            #print(title)
+            driver.get(current_page)
+
+#scrape_all()
+#scrape_all_min()
+#scrape_today()
 
 driver.quit()
