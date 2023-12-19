a6af52bc92c4c6c95b23f04f42d9d7a6b263f73a
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2023-12-19 15:38:32 +0900

    делаю

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from aiogram.filters.command import Command
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import datetime
+import time
 
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-# Объект бота
+
 bot_token = open("bot_token", "r")
 bot = Bot(token=bot_token.read())
-# Диспетчер
+
 dp = Dispatcher()
 
 urls = []
@@ -23,15 +23,17 @@ cService = webdriver.ChromeService(executable_path='./chromedriver.exe')
 driver = webdriver.Chrome(service=cService)
 
 def scrape_today():
-    #time_now = datetime.datetime.now()
-    time_now = datetime.datetime.strptime('27.11.2023 16:00:00', '%d.%m.%Y %H:%M:%S')
+    global urls, titles
+    #time_now = datetime.date.today()
+    #time_now = datetime.datetime.strptime('27.11.2023 16:00:00', '%d.%m.%Y %H:%M:%S')
+    time_now = datetime.datetime.strptime('27.11.2023', '%d.%m.%Y')
     current_page = website
     for i in range(1, 28):
         current_page = current_page.replace('?PAGEN_1=' + current_page[-1], '?PAGEN_1=' + str(i))
         driver.get(current_page)
         articles = driver.find_elements(By.XPATH, "//*[@id='content']/div[1]/div")
-        urls = []
-        titles = []
+        #urls = []
+        #titles = []
         for j in range(2, len(articles)):
             article = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div["+str(j)+"]/article/div[1]/a[1]").get_attribute('href')
             driver.get(article)
@@ -56,10 +58,10 @@ def scrape_today():
             #print(title)
             driver.get(current_page)
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    #await message.answer("Hello!")
+    await message.answer(str(titles[0]) + '\n' + str(urls[0]))
 
 @dp.message(Command("startscrape"))
 async def cmd_start(message: types.Message):
@@ -70,12 +72,24 @@ async def cmd_start(message: types.Message):
 async def cmd_start(message: types.Message):
     await message.answer(str(titles[0]) + '\n' + str(urls[0]))
 
+async def scraperesult():
+    await bot.send_message(all, str(titles[0]) + '\n' + str(urls[0]))
 
-# Запуск процесса поллинга новых апдейтов
+async def scheduled(wait_for):
+  while True:
+    await asyncio.sleep(wait_for)
+
+    scrape_today()
+
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    #loop = asyncio.get_event_loop()
+    #loop.create_task(scheduled(10))
+    scrape_today()
+    #time.sleep(2)
+    scraperesult()
     asyncio.run(main())
 
 driver.quit()
